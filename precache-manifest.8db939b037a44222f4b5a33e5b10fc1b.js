3c30bbbefd0fe99a308af9064d90e44806fe8dff
MhKh78 <mhkh431@gmail.com>
2019-08-13 21:10:13 +0430

    Updates

---
+++ precache-manifest.8db939b037a44222f4b5a33e5b10fc1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c2891da18c660bba99e318503cc48d",
+    "url": "/MhKh78/mhkh-monsters/index.html"
+  },
+  {
+    "revision": "fe4830905299be93acd7",
+    "url": "/MhKh78/mhkh-monsters/static/css/main.7eb1551c.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/MhKh78/mhkh-monsters/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "fe4830905299be93acd7",
+    "url": "/MhKh78/mhkh-monsters/static/js/main.b6231624.chunk.js"
+  },
+  {
+    "revision": "3409c04591f06598ca8e",
+    "url": "/MhKh78/mhkh-monsters/static/js/runtime~main.e95ffb6e.js"
+  }
+]);
