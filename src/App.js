e500d396e83483037d0b3a69821d88dbb59d3516
MhKh78 <mhkh431@gmail.com>
2019-08-13 14:20:35 +0430

    Robo Pics Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -28,9 +29,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {this.state.monsters.map(monster => (
-            <h1 key={monster.id}> {monster.name} </h1>
-          ))}
+          <CardList monsters={this.state.monsters} />
         </header>
       </div>
     );
