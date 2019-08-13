e760a6d765a79ed1e0d63b39aa6813c17c57a1d8
MhKh78 <mhkh431@gmail.com>
2019-08-13 13:46:51 +0430

    Monsters Added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload ss.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // This Way
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  // Or This Way
+  /*
+  state = {
+    string: 'Hello Yihua'
+  };
+   */
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}> {monster.name} </h1>
+          ))}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
